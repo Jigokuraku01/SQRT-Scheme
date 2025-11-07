a848192a54b7f13137bf680cb7b36a6ee6b97d1f
Andrey Kashenin <and.kashenin@mail.ru>
2025-11-07 15:00:45 +0300

    fixed main logic of mant calc in verilog

--- sqrt2.sv
+++ sqrt2.sv
@@ -21,22 +21,57 @@ module sqrt2 (
   reg [21:0] remainder;
   reg [10:0] root;
   reg [21:0] tmp_root;
-  reg [3:0] sqrt_iter_cnt;
+  integer sqrt_iter_cnt;
+  reg [3:0] negedge_count;
 
   reg [16:0] special_check;
   reg is_special;
 
-  reg [15:0] out_data;
+
+  reg result_reg;
+  reg isnan_reg;
+  reg ispinf_reg;
+  reg isninf_reg;
+
+  assign RESULT  = result_reg;
+  assign IS_NAN  = isnan_reg;
+  assign IS_PINF = ispinf_reg;
+  assign IS_NINF = isninf_reg;
+
+  reg [15:0] out_data = 16'bz;
   assign IO_DATA = out_data;
 
   reg [2:0] Start = 3'd0;
   reg [2:0] Calculate = 3'd1;
   reg [2:0] Done = 3'd2;
 
+  function void do_tick();
+    begin
+      remainder = {remainder[19:0], mantissa_big[21:20]};
+      tmp_root  = {root << 2, 2'b01};
+      if (tmp_root <= remainder)
+      begin
+        remainder = remainder - tmp_root;
+        root = (root << 1) | 1'b1;
+      end
+      else
+      begin
+        root = root << 1;
+      end
+      ans_mantissa = root[9:0];
+      if (sqrt_iter_cnt == 11)
+      begin
+        state = Done;
+      end
+      mantissa_big  = mantissa_big << 2;
+      sqrt_iter_cnt = sqrt_iter_cnt + 1;
+    end
+  endfunction
+
   function automatic [26:0] prepare_mantissa_and_exponent;
     input [9:0] mant;
     input [4:0] exp;
-    reg [9:0] ans_mant;
+    reg [21:0] ans_mant;
     reg [4:0] ans_exp;
     integer pos_of_last_one;
     integer i;
@@ -47,9 +82,11 @@ module sqrt2 (
       if (exp != 0)
       begin
         ans_exp  = (exp + 15) >> 1;
-        ans_mant = mant | 10'b1000000000;
+        ans_mant = mant | 11'b10000000000;
         if (exp[0] == 0)
+        begin
           ans_mant = ans_mant << 1;
+        end
       end
       else
       begin
@@ -68,6 +105,7 @@ module sqrt2 (
       end
       ans_mant = ans_mant << 10;
       prepare_mantissa_and_exponent = {ans_mant, ans_exp};
+      $display("%b", mant);
     end
   endfunction
 
@@ -110,12 +148,21 @@ module sqrt2 (
     end
   endfunction
 
+
   always @(posedge CLK)
   begin
-    if (sqrt_iter_cnt > 2)
+    if (ENABLE && negedge_count >= 2)
     begin
-      ans_mantissa = root[9:0];
-      out_data     = {ans_sign, ans_exponent, ans_mantissa};
+      if(state == Calculate)
+      begin
+        do_tick();
+        out_data <= {ans_sign, ans_exponent, ans_mantissa};
+      end
+      else if(state == Done)
+      begin
+        out_data <= {ans_sign, ans_exponent, ans_mantissa};
+        result_reg <= 1;
+      end
     end
   end
   always @(negedge CLK)
@@ -123,10 +170,10 @@ module sqrt2 (
     if (!ENABLE)
     begin
       state         = Start;
-      RESULT        = 0;
-      IS_NAN        = 0;
-      IS_PINF       = 0;
-      IS_NINF       = 0;
+      result_reg    = 0;
+      isnan_reg     = 0;
+      ispinf_reg    = 0;
+      isninf_reg    = 0;
       sqrt_iter_cnt = 0;
       root          = 0;
       remainder     = 0;
@@ -139,85 +186,65 @@ module sqrt2 (
       inp_mantissa  = 0;
       inp_exp       = 0;
       mantissa_big  = 0;
+      negedge_count = 0;
       out_data      = 16'bz;
     end
     else
     begin
+      if (negedge_count <= 2 && negedge_count > 0)
+      begin
+        out_data = {ans_sign, ans_exponent, ans_mantissa};
+        negedge_count = negedge_count + 1;
+      end
       case (state)
         Start:
         begin
-          inp_data      = IO_DATA;
-          inp_exp       = IO_DATA[14:10];
-          inp_mantissa  = IO_DATA[9:0];
-
+          inp_data = IO_DATA;
           special_check = check_special_cases(inp_data);
-          is_special    = special_check[16];
-          pos_result    = special_check[15:0];
-
+          is_special = special_check[16];
+          pos_result = special_check[15:0];
           if (is_special)
           begin
-            ans_sign = inp_data[15];
+            ans_sign = pos_result[15];
             ans_exponent = pos_result[14:10];
             ans_mantissa = pos_result[9:0];
-            out_data = pos_result;
-            RESULT = 1;
-            IS_NAN       = (pos_result[14:10] == 5'h1F && pos_result[9:0] != 0) ||
-            (pos_result[15] && (pos_result[14:10] != 0 || pos_result[9:0] != 0));
-            IS_PINF = (pos_result[14:10] == 5'h1F && pos_result[9:0] == 0 && !pos_result[15]);
-            IS_NINF = 0;
             state = Done;
+            result_reg = 1;
+            isnan_reg = (ans_exponent == 5'h1F && ans_mantissa != 0) ||
+            (ans_sign && (ans_exponent != 0 || ans_mantissa != 0));
+            isninf_reg = 0;
+            ispinf_reg = (ans_exponent == 5'h1F && ans_mantissa == 0 && !ans_sign);
           end
           else
           begin
-            ans_sign                     = 0;
+            inp_mantissa                 = inp_data[9:0];
+            inp_exp                      = inp_data[14:10];
             {mantissa_big, ans_exponent} = prepare_mantissa_and_exponent(inp_mantissa, inp_exp);
-            root                         = 0;
+            ans_sign                     = 0;
             remainder                    = 0;
+            root                         = 0;
             sqrt_iter_cnt                = 0;
-            ans_mantissa                 = root[9:0];
-            out_data                     = {ans_sign, ans_exponent, ans_mantissa};
             state                        = Calculate;
           end
         end
-
         Calculate:
         begin
-          if (sqrt_iter_cnt == 12)
+          if(sqrt_iter_cnt < 2)
           begin
-            state  = Done;
-            RESULT = 1;
-          end
-          else
-          begin
-            tmp_root = (root << 2) | 1;
-            if (tmp_root <= (remainder << 2 | mantissa_big[21:20]))
-            begin
-              root      = (root << 1) | 1;
-              remainder = (remainder << 2 | mantissa_big[21:20]) - tmp_root;
-            end
-            else
-            begin
-              root      = root << 1;
-              remainder = (remainder << 2 | mantissa_big[21:20]);
-            end
-            mantissa_big  = mantissa_big << 2;
-            sqrt_iter_cnt = sqrt_iter_cnt + 1;
-
-            if (sqrt_iter_cnt <= 2)
-            begin
-              ans_mantissa = root[9:0];
-              if (sqrt_iter_cnt <= 2)
-              begin
-                out_data = {ans_sign, ans_exponent, ans_mantissa};
-              end
-            end
+            do_tick();
+            out_data = {ans_sign, ans_exponent, ans_mantissa};
           end
         end
 
         Done:
         begin
-          RESULT = 1;
+          if (sqrt_iter_cnt < 2)
+          begin
+            out_data = {ans_sign, ans_exponent, ans_mantissa};
+          end
+          result_reg = 1;
         end
+
       endcase
     end
   end
